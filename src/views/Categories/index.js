a86690e002b1877cbe17254977840eeb4e9ad8b7
Afonso Nunes Rodrigues <afonso.rodrigues@MLZPC-1062.local>
2021-12-01 11:17:22 -0300

    css

--- src/views/Categories/index.js
+++ src/views/Categories/index.js
@@ -1,43 +1,42 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
-import Loader from '../../assets/loader.gif';
+import Loader from "../../assets/loader.gif";
 
 const Categories = () => {
-  const [joke, setJoke] = useState({});
-  const [ isLoad, setIsLoad ] = useState(false)
-  const { category } = useParams();
+    const [joke, setJoke] = useState({});
+    const [isLoad, setIsLoad] = useState(false);
+    const { category } = useParams();
 
-  useEffect(() => {
-    setIsLoad(true)
-    api.get(`random?category=${category}`).then(
-      res => {
-        setJoke(res.data)
-      }
-    )
-    .catch( err => console.error(err) )
-    .finally( () => setIsLoad(false))
-  }, [category])
+    useEffect(() => {
+        setIsLoad(true);
+        api.get(`random?category=${category}`)
+            .then((res) => {
+                setJoke(res.data);
+            })
+            .catch((err) => console.error(err))
+            .finally(() => setIsLoad(false));
+    }, [category]);
 
-  if(isLoad){
-    return(
-      <div>
-        <img src={Loader} alt="loader" />
-      </div>
-    )
-  }
+    if (isLoad) {
+        return (
+            <div className="loader">
+                <img src={Loader} alt="loader" />
+            </div>
+        );
+    }
 
-  return (
-    <>
-      <h1>Categorias</h1>
-      <div>
-        <img src={joke?.icon_url} alt={joke?.value}/>
-        <h4>{joke?.value}</h4>
-      </div>
-    </>
-  )
-}
+    return (
+        <div className="home-component">
+            <h1>Categorias</h1>
+            <div className="jokes">
+                <img src={joke?.icon_url} alt={joke?.value} />
+                <h4>{joke?.value}</h4>
+            </div>
+        </div>
+    );
+};
 
-export default Categories
+export default Categories;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,83 +1,88 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
-import {
-  FormLabel,
-  Input,
-  Button
-} from '@chakra-ui/react'
+import { Input, Button } from "@chakra-ui/react";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
-import Loader from '../../assets/loader.gif'
+import Loader from "../../assets/loader.gif";
 
 const App = () => {
-  const [data, setData] = useState({});
-  const [allJokes, setAllJokes] = useState({});
-  const [isSearch, setIsSearch] = useState(false);
-  const [isLoad, setIsLoad] = useState(false);
-  const [searchJoke, setSearchJoke] = useState('');
+    const [data, setData] = useState({});
+    const [allJokes, setAllJokes] = useState({});
+    const [isSearch, setIsSearch] = useState(false);
+    const [isLoad, setIsLoad] = useState(false);
+    const [searchJoke, setSearchJoke] = useState("");
 
-  useEffect(() => {
-    setIsLoad(true)
-    api.get('random').then(
-      response => {
-        setData(response.data)
-      }
-    )
-    .catch( e => console.error(e))
-    .finally( () => setTimeout(() => {
-      setIsLoad(false)
-    }, 2500))
-  }, [])
+    useEffect(() => {
+        setIsLoad(true);
+        api.get("random")
+            .then((response) => {
+                setData(response.data);
+            })
+            .catch((e) => console.error(e))
+            .finally(() =>
+                setTimeout(() => {
+                    setIsLoad(false);
+                }, 2500)
+            );
+    }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsLoad(true)
-    api.get(`search?query=${searchJoke}`).then(
-      res => {
-        setIsSearch(true)
-        setAllJokes(res.data)
-      }
-    )
-    .catch( err => console.error(err))
-    .finally( () => setIsLoad(false))
-  }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsLoad(true);
+        api.get(`search?query=${searchJoke}`)
+            .then((res) => {
+                setIsSearch(true);
+                setAllJokes(res.data);
+            })
+            .catch((err) => console.error(err))
+            .finally(() => setIsLoad(false));
+    };
 
-  if(isLoad) {
-    return(
-      <div className="loader">
-        <img src={Loader} alt="Loader" />
-      </div>
-    )
-  }
-
-  return(
-    <div className="home-component">
-      <h1>Joke</h1>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <FormLabel>Pesquise sua piada</FormLabel>
-          <Input type="text" onChange={e => setSearchJoke(e.target.value)} />
-          <Button type="submit">Pesquisar</Button>
-        </form>
-      </div>
-      { !isSearch ? (
-        <div className="jokes">
-          <img src={data?.icon_url} alt={data?.value} />
-          <h3>{data?.value}</h3>
-        </div>
-      ) : (
-        <>
-          { allJokes?.result.map( (item, index) => (
-            <div key={index} className="jokes">
-              <img src={item?.icon_url} alt={item?.value} />
-              <h3>{item?.value}</h3>
+    if (isLoad) {
+        return (
+            <div className="loader">
+                <img src={Loader} alt="Loader" />
             </div>
-          ))}
-        </>
-      )}
-    </div>
-  )
-}
+        );
+    }
 
-export default App
+    return (
+        <div className="home-component">
+            <h1>Joke</h1>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <Input
+                        placeholder="Pesquise sua piada aqui"
+                        type="text"
+                        onChange={(e) => setSearchJoke(e.target.value)}
+                    />
+                    <Button className="buttonPesquisar" type="submit">
+                        Pesquisar
+                    </Button>
+                </form>
+            </div>
+            {!isSearch ? (
+                <div className="jokes">
+                    <img src={data?.icon_url} alt={data?.value} />
+                    <h3>{data?.value}</h3>
+                </div>
+            ) : (
+                <>
+                    {allJokes?.result.map((item, index) => (
+                        <div key={index} className="jokes">
+                            <div>
+                                <img src={item?.icon_url} alt={item?.value} />
+                            </div>
+                            <div>
+                                <h3>{item?.value}</h3>
+                            </div>
+                        </div>
+                    ))}
+                </>
+            )}
+        </div>
+    );
+};
+
+export default App;
